d191db06e27ad27848c1318b7722468ac21dbd8a
tanviag2601 <tanvi.agarwal.2601@gmail.com>
2026-03-28 16:11:47 +0530

    To view tables according to the months

--- view.py
+++ view.py
@@ -1,10 +1,13 @@
 import sqlite3
+from store import create_table
 from tabulate import tabulate
-conne = sqlite3.connect("expenses.db")  #creating database file
-cursor = conne.cursor()
-# to view the table
-cursor.execute("""SELECT * FROM Expense""")
-rows= cursor.fetchall()
-headers=[i[0] for i in cursor.description]
-print(tabulate(rows, headers=headers, tablefmt="grid"))
-conne.close()
+def view_table(month):
+    conne = sqlite3.connect("expenses.db")  #creating database file
+    cursor = conne.cursor()
+    # to view the table
+    cursor.execute(f"""SELECT * FROM {month}""")
+    rows= cursor.fetchall()
+    headers=[i[0] for i in cursor.description]
+    print("MONTH: ",month)
+    print(tabulate(rows, headers=headers, tablefmt="grid"))
+    conne.close()
